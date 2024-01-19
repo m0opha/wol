b8bca6b5881c3d0f4bd8a1be85f2738a89e528aa
m0opha <142889788+m0opha@users.noreply.github.com>
2024-01-19 03:48:02 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from .functions import ArgHandler, ReadFile, SendMagicPacket
 
 def main():
     Total_MACs={}
-    macs = {}
+    MACs = {}
     exMACs, file_paths = ArgHandler()
     
     for _path in file_paths:
         macs.update(ReadFile(_path))
     
     Total_MACs.update(exMACs)
-    Total_MACs.update(macs)
+    Total_MACs.update(MACs)
     SendMagicPacket(Total_MACs)
 
 if __name__ == "__main__":
